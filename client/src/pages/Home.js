17f33f3b22083dfcfdfc316f949b9d556b84fa7c
RamyCodes <87505205+RamyCodes@users.noreply.github.com>
2022-06-07 11:42:19 +0200

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,13 +4,13 @@ import Categories from '../Components/Categories'
 import Navbar from '../Components/Navbar'
 import ProductList from '../Components/ProductList'
 import CategoryItem from '../Components/CategoryItem'
-import axios from 'axios'
+import {axiosInstance} from '../config'
 
 const Home = () => {
   console.log("currentUser order:" + sessionStorage.getItem('currentUser') );
     if(sessionStorage.getItem('currentUser') === null){
       const DeleteOrders = () =>{
-        axios.delete(`http://localhost:5000/api/orders`)
+        axiosInstance.delete(`http://localhost:5000/api/orders`)
         .then( res => {
           console.log(res)
         })
